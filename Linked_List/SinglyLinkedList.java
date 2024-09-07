3a11d6040ce346b3eb9c634b113218c6ed76a846
joel <joelroyskroys@gmail.com>
2024-09-07 21:50:45 +0530

    SinglyLinkedList

--- Linked_List/SinglyLinkedList.java
+++ Linked_List/SinglyLinkedList.java
@@ -152,7 +152,7 @@ public class SinglyLinkedList{
             Node next=null;
             while(current!=null){
                 next=current.next;
-                current.next=previous;
+                current.next=previous; 
                 previous=current;
                 current=next;
             }
